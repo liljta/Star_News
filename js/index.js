77955bc41c332a7bc57892bed7e96c2932d0b98c
Liliya <liljsik1@gmail.com>
2019-04-18 10:07:48 +0300

    currency

--- js/index.js
+++ js/index.js
@@ -77,3 +77,69 @@ $('.nav.nav-tabs a').on('click', function (e) {
     tabPanel.innerHTML = 'Загрузка';
     Request($(this).data('category'), callbackFunction);
 });
+
+function PBRequest(card = false) {
+    var xhr = new XMLHttpRequest();
+
+    if (card) {
+        var url = "https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11";
+    } else {
+        var url = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5";
+    }
+
+    xhr.open("GET", url, true);
+    //xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState != 4) return;
+
+        if (xhr.status != 200) {
+            var errStatus = xhr.status;
+            var errText = xhr.statusText;
+            console.log(errStatus + ": " + errText);
+        } else {
+            var data = JSON.parse(xhr.responseText);
+            ShowCurrencyTable(data, card);
+            //var data = xhr.responseText;
+            //console.log(data);
+        }
+    };
+}
+
+function ShowCurrencyTable(data, card) {
+    if (card) {
+        var currencyTbody = document.querySelector(".currency-exchange-card");
+    } else {
+        var currencyTbody = document.querySelector(".currency-exchange");
+    }
+
+    for(var i = 0; i < data.length; i++){
+        console.log(` ${data[i].ccy} / ${data[i].base_ccy} Buy ${data[i].buy} Sell ${data[i].sale} `);
+        var tr = document.createElement('tr');
+        var tdCurrencyName = document.createElement('td');
+        tdCurrencyName.innerText = `${data[i].ccy} / ${data[i].base_ccy}`;
+        tr.appendChild(tdCurrencyName);
+        var tdCurrencyBid = document.createElement('td');
+        tdCurrencyBid.innerText = data[i].buy;
+        tr.appendChild(tdCurrencyBid);
+        var tdCurrencyAsk = document.createElement('td');
+        tdCurrencyAsk.innerText = data[i].sale;
+        tr.appendChild(tdCurrencyAsk);
+        currencyTbody.appendChild(tr);
+    }
+}
+
+window.onload = function() {
+    PBRequest();
+    PBRequest(true);
+};
+
+function DayNight(){
+    var page = document.querySelector(".page");
+    if (page && page.classList) {
+        page.classList.toggle("night");
+    }
+}
+
